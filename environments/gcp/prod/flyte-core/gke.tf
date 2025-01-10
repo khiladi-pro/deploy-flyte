f936b12e24a40ba7eb2fe8fb5b940b82df11b39c
kishorekumarsingh <kumarsingh14.kishore@gmail.com>
2025-01-10 10:26:17 +0530

    updated node count and location

--- environments/gcp/prod/flyte-core/gke.tf
+++ environments/gcp/prod/flyte-core/gke.tf
@@ -25,9 +25,6 @@ module "gke" {
       machine_type      = "e2-standard-8"
       disk_size_gb      = 100
       enable_gcfs       = true
-      total_min_count   = 0
-      total_max_count   = 3
-      node_locations    = "asia-south1-a"
     },
     {
       name              = "default-gpu"
@@ -35,9 +32,6 @@ module "gke" {
       disk_size_gb      = 100
       enable_gcfs       = true
       disk_type         = "pd-ssd"  
-      total_min_count   = 0
-      total_max_count   = 2
-      node_locations    = "asia-south1-b"
     },
     {
       name              = "default"
